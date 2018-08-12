5b73e510cd26bee9caa87ee372b76f5eafb4e892
yuri <y.kuchanov@redmadrobot.com>
2018-08-13 01:18:37 +0300

    - add gallery/files/** to ignored by security urls

--- src/main/java/ru/kuchanov/scpreaderapi/configuration/WebSecurityConfiguration.kt
+++ src/main/java/ru/kuchanov/scpreaderapi/configuration/WebSecurityConfiguration.kt
@@ -123,7 +123,7 @@ class WebSecurityConfiguration : WebSecurityConfigurerAdapter() {
     }
 
     override fun configure(web: WebSecurity) {
-        web.ignoring().antMatchers("*.bundle.*")
+        web.ignoring().antMatchers("/gallery/files/**")
     }
 
     @Bean
