71111251f3fae17d44f429ecf9ef58ee46b0ea43
yuri <y.kuchanov@redmadrobot.com>
2018-09-17 22:52:32 +0300

    - some progress

---
+++ src/main/java/ru/kuchanov/scpreaderapi/bean/articles/FirebaseDataUpdateDate.kt
@@ -0,0 +1,18 @@
+package ru.kuchanov.scpreaderapi.bean.articles
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+import javax.persistence.*
+
+@Entity
+@Table(name = "articles")
+data class Article(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long? = null,
+        @Column(name = "article_type_id")
+        val articleTypeId: Long? = null
+)
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such article")
+class ArticleNotFoundException : RuntimeException()
