c0cfef47533bc340b121dd1cd05927fa3cd7b84a
yuri <y.kuchanov@redmadrobot.com>
2018-09-25 22:52:14 +0300

    - fix crash while try to fill leaderboardUser model from db if avatar is null

--- src/main/java/ru/kuchanov/scpreaderapi/model/user/LeaderboardUser.kt
+++ src/main/java/ru/kuchanov/scpreaderapi/model/user/LeaderboardUser.kt
@@ -2,7 +2,7 @@ package ru.kuchanov.scpreaderapi.model.user
 
 data class LeaderboardUser(
         val id: Long,
-        val avatar: String,
+        val avatar: String?,
         var fullName: String?,
         var score: Int,
         //level
