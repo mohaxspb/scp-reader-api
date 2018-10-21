d74878e7f52ba6e280383fea8cd91ebc0c6d26c8
yuri <y.kuchanov@redmadrobot.com>
2018-10-21 18:42:06 +0300

    - reenable correct timestamp representation

--- src/main/java/ru/kuchanov/scpreaderapi/configuration/NetworkConfiguration.kt
+++ src/main/java/ru/kuchanov/scpreaderapi/configuration/NetworkConfiguration.kt
@@ -11,6 +11,7 @@ import retrofit2.Converter
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.jackson.JacksonConverterFactory
 import ru.kuchanov.scpreaderapi.network.ApiClient
+import java.text.SimpleDateFormat
 import java.util.concurrent.TimeUnit
 
 @Configuration
@@ -33,7 +34,10 @@ class NetworkConfiguration {
     fun callAdapterFactory(): RxJava2CallAdapterFactory = RxJava2CallAdapterFactory.create()
 
     @Bean
-    fun objectMapper(): ObjectMapper = ObjectMapper().registerKotlinModule().enable(SerializationFeature.INDENT_OUTPUT)
+    fun objectMapper(): ObjectMapper = ObjectMapper()
+            .registerKotlinModule()
+            .enable(SerializationFeature.INDENT_OUTPUT)
+            .setDateFormat(SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
 
     @Bean
     fun converterFactory(): Converter.Factory = JacksonConverterFactory.create(objectMapper())
