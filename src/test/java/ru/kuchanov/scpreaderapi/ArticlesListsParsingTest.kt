685f9e6273d959c121994f05029de3848f4b1ba1
mohax <mohax.spb@gmail.com>
2019-11-28 19:38:58 +0300

    Add test for object articles parsing for RU lang.

--- src/test/java/ru/kuchanov/scpreaderapi/ArticlesListsParsingTest.kt
+++ src/test/java/ru/kuchanov/scpreaderapi/ArticlesListsParsingTest.kt
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import ru.kuchanov.scpreaderapi.bean.articles.ArticleForLang
+import ru.kuchanov.scpreaderapi.bean.users.Lang
 import ru.kuchanov.scpreaderapi.service.parse.ArticleParsingServiceBase
 import ru.kuchanov.scpreaderapi.service.users.LangService
 
@@ -101,9 +103,68 @@ class ArticlesListsParsingTest {
                 .toList()
                 .test()
                 .assertValue { allArticlesForLangs ->
-                    val notEmptyArticlesListsWithoutItaly = allArticlesForLangs.filter { it.isNotEmpty() }
+                    val notEmptyArticlesLists = allArticlesForLangs.filter { it.isNotEmpty() }
                     val allLangs = ScpReaderConstants.Firebase.FirebaseInstance.values()
-                    notEmptyArticlesListsWithoutItaly.size == allLangs.size
+                    notEmptyArticlesLists.size == allLangs.size
+                }
+                .assertNoErrors()
+                .assertComplete()
+    }
+
+    @Test
+    fun getObjectArticles_receivesNotEmptyList() {
+        Flowable
+                .fromIterable(ScpReaderConstants.Firebase.FirebaseInstance.values().toList())
+                //todo remove filter after all lang realization
+                .filter { it == ScpReaderConstants.Firebase.FirebaseInstance.RU }
+                .map { langService.getById(it.lang)!! }
+                .flatMapSingle { lang ->
+                    val objectUrls = articleParsingServiceBase
+                            .getParsingRealizationForLang(lang)
+                            .getObjectArticlesUrls()
+                            .map { Pair(lang, it) }
+                    Flowable
+                            .fromIterable(objectUrls)
+                            .flatMapSingle { langAndObjectUrl ->
+                                articleParsingServiceBase
+                                        .getParsingRealizationForLang(langAndObjectUrl.first)
+                                        .getObjectsArticlesForLang(langAndObjectUrl.first, langAndObjectUrl.second)
+                                        .map { Triple(langAndObjectUrl.first, langAndObjectUrl.second, it) }
+                            }
+                            .toList()
+                }
+                .doOnError { println(it) }
+                .toList()
+                .doOnSuccess { println("doOnSuccess ${it.size}") }
+                .test()
+                //assert that every lang has at least some articles for every object link
+                .assertValue { listOfObjectListsForEveryLang: MutableList<MutableList<Triple<Lang, String, List<ArticleForLang>>>> ->
+                    val notEmptyArticlesLists =
+                            listOfObjectListsForEveryLang
+                                    //remove if no object urls were found
+                                    .filter { it.isNotEmpty() }
+                                    //remove if no articles found for object url for this lang
+                                    .filter { listOfAllObjectArticleForLang: MutableList<Triple<Lang, String, List<ArticleForLang>>> ->
+                                        val notEmptyObjectsListsForLang =
+                                                listOfAllObjectArticleForLang
+                                                        .filter { langAndObjectUrlAndArticles: Triple<Lang, String, List<ArticleForLang>> ->
+                                                            val lang = langAndObjectUrlAndArticles.first.langCode
+                                                            val url = langAndObjectUrlAndArticles.second
+                                                            val articlesCount = langAndObjectUrlAndArticles.third.size
+                                                            println("Articles for $lang ($url): $articlesCount")
+                                                            langAndObjectUrlAndArticles.second.isNotEmpty()
+                                                        }
+                                        val numOfObjectLinks = articleParsingServiceBase
+                                                .getParsingRealizationForLang(listOfAllObjectArticleForLang.first().first)
+                                                .getObjectArticlesUrls().size
+                                        println("numOfObjectLinks: $numOfObjectLinks")
+                                        println("notEmptyObjectsListsForLang: ${notEmptyObjectsListsForLang.size}")
+                                        notEmptyObjectsListsForLang.size == numOfObjectLinks
+                                    }
+                    val allLangs = ScpReaderConstants.Firebase.FirebaseInstance.values()
+                            //fixme add other langs
+                            .filter { it == ScpReaderConstants.Firebase.FirebaseInstance.RU }
+                    notEmptyArticlesLists.size == allLangs.size
                 }
                 .assertNoErrors()
                 .assertComplete()
