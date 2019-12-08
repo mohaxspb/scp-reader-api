4c4252dfc69bd99be4c74bfba936f4ce14ede46a
mohax <mohax.spb@gmail.com>
2019-12-08 19:26:09 +0300

    Refactoring.

--- src/main/java/ru/kuchanov/scpreaderapi/service/article/ArticleForLangServiceImpl.kt
+++ src/main/java/ru/kuchanov/scpreaderapi/service/article/ArticleForLangServiceImpl.kt
@@ -8,7 +8,6 @@ import ru.kuchanov.scpreaderapi.repository.article.ArticlesImagesRepository
 import ru.kuchanov.scpreaderapi.repository.article.tags.TagForLangRepository
 
 
-@Suppress("unused")
 @Service
 class ArticleForLangServiceImpl : ArticleForLangService {
 
@@ -39,29 +38,33 @@ class ArticleForLangServiceImpl : ArticleForLangService {
     //FUCKING SHIT!!!!!!!!!!!!!!!!!!!!
     //todo try overrided pageable with offset/limit support
     override fun getMostRecentArticlesForLang(langId: String, offset: Int, limit: Int) =
-            articlesForLangRepository.getMostRecentArticlesForLang(langId, offset, limit).map {
-                //shit is here, blyat
-                it.apply {
-                    imageUrls = imagesRepository.findAllByArticleForLangId(articleForLangId = it.id)
-                    tagsForLang = tagsForLangRepository.getAllForLangIdAndArticleForLangIdAsDto(
-                            langId = langId,
-                            articleForLangId = it.id
-                    )
-                }
-            }
+            articlesForLangRepository
+                    .getMostRecentArticlesForLang(langId, offset, limit)
+                    .map {
+                        //shit is here, blyat
+                        it.apply {
+                            imageUrls = imagesRepository.findAllByArticleForLangId(articleForLangId = it.id)
+                            tagsForLang = tagsForLangRepository.getAllForLangIdAndArticleForLangIdAsDto(
+                                    langId = langId,
+                                    articleForLangId = it.id
+                            )
+                        }
+                    }
 
     override fun getMostRecentArticlesForLangFull(langId: String, offset: Int, limit: Int) =
-            articlesForLangRepository.getMostRecentArticlesForLangFull(langId, offset, limit).map {
-                it.apply {
-                    images = imagesRepository.findAllByArticleForLangIdFull(articleForLangId = id!!).toMutableSet()
-                    tags = tagsForLangRepository.getAllForLangIdAndArticleForLangId(
-                            langId = langId,
-                            articleForLangId = id
-                    ).toMutableSet()
-                }
-            }
+            articlesForLangRepository
+                    .getMostRecentArticlesForLangFull(langId, offset, limit)
+                    .map {
+                        it.apply {
+                            images = imagesRepository.findAllByArticleForLangIdFull(articleForLangId = id!!).toMutableSet()
+                            tags = tagsForLangRepository.getAllForLangIdAndArticleForLangId(
+                                    langId = langId,
+                                    articleForLangId = id
+                            ).toMutableSet()
+                        }
+                    }
 
-    override fun findAllArticlesForLangByArticleCategoryToLangId(articleCategoryToLangId: Long): List<ArticleForLang> =
+    override fun findAllArticlesForLangByArticleCategoryToLangId(articleCategoryToLangId: Long) =
             articlesForLangRepository.findAllArticlesForLangByArticleCategoryToLangId(articleCategoryToLangId)
 
     override fun getOneByLangAndArticleId(articleId: Long, langId: String) =
