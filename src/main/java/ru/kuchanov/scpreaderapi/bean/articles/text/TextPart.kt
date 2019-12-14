0a940c1998fc63da46c8dc93557241822b5a7f1c
mohax <mohax.spb@gmail.com>
2019-12-14 16:49:04 +0300

    Add entity for article text part table.

---
+++ src/main/java/ru/kuchanov/scpreaderapi/bean/articles/text/TextPart.kt
@@ -0,0 +1,31 @@
+package ru.kuchanov.scpreaderapi.bean.articles.text
+
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import ru.kuchanov.scpreaderapi.service.parse.article.TextType
+import java.sql.Timestamp
+import javax.persistence.*
+
+@Entity
+@Table(name = "article_to_lang_text_parts")
+data class TextPart(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long? = null,
+        @Column(name = "article_to_lang_id")
+        var articleToLangId: Long,
+        @Column(name = "parent_id")
+        var parentId: Long? = null,
+        @Enumerated(EnumType.STRING)
+        @Column(columnDefinition = "TEXT")
+        val type: TextType,
+        @Column(columnDefinition = "TEXT")
+        val data: String,
+        @Column(name = "order_in_text")
+        var orderInText: Long,
+
+        @field:CreationTimestamp
+        val created: Timestamp? = null,
+        @field:UpdateTimestamp
+        val updated: Timestamp? = null
+)
