fd290775f0bc6f346d1aa72d42a962185b763986
mohax <mohax.spb@gmail.com>
2019-12-17 18:50:39 +0300

    Attempt to fix NoArgConstructor.

--- src/main/java/ru/kuchanov/scpreaderapi/utils/NoArgConstructor.kt
+++ src/main/java/ru/kuchanov/scpreaderapi/utils/NoArgConstructor.kt
@@ -1,6 +1,12 @@
 package ru.kuchanov.scpreaderapi.utils
 
+
+import kotlin.annotation.AnnotationRetention.RUNTIME
+import kotlin.annotation.AnnotationTarget.CLASS
+
 /**
  * used to mark classes for which default constructor must be created
  */
+@Target(CLASS)
+@Retention(RUNTIME)
 annotation class NoArgConstructor
