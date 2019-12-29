ca34046735d908bc6b43c1c80956149d40c79758
mohax <mohax.spb@gmail.com>
2019-12-29 18:36:45 +0300

    Realize getting favorite articles for user and lang. Refactoring.

---
+++ src/main/java/ru/kuchanov/scpreaderapi/controller/article/FavoriteArticleController.kt
@@ -0,0 +1,76 @@
+package ru.kuchanov.scpreaderapi.controller.article
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.*
+import ru.kuchanov.scpreaderapi.ScpReaderConstants
+import ru.kuchanov.scpreaderapi.bean.articles.favorite.FavoriteArticleByLang
+import ru.kuchanov.scpreaderapi.bean.articles.favorite.FavoriteArticleByLangAlreadyExistsException
+import ru.kuchanov.scpreaderapi.bean.articles.favorite.FavoriteArticleByLangNotFoundException
+import ru.kuchanov.scpreaderapi.bean.users.LangNotFoundException
+import ru.kuchanov.scpreaderapi.bean.users.User
+import ru.kuchanov.scpreaderapi.model.dto.article.ReadOrFavoriteArticleToLangDto
+import ru.kuchanov.scpreaderapi.service.article.favorite.FavoriteArticleForLangService
+import ru.kuchanov.scpreaderapi.service.users.LangService
+
+
+@RestController
+@RequestMapping("/" + ScpReaderConstants.Path.ARTICLE + "/" + ScpReaderConstants.Path.FAVORITE)
+class FavoriteArticleController @Autowired constructor(
+        val favoriteArticleForLangService: FavoriteArticleForLangService,
+        val langService: LangService
+) {
+
+    @GetMapping("all")
+    fun all(
+            @RequestParam(value = "langEnum") langEnum: ScpReaderConstants.Firebase.FirebaseInstance?,
+            @RequestParam(value = "offset") offset: Int,
+            @RequestParam(value = "limit") limit: Int,
+            @AuthenticationPrincipal user: User
+    ): List<ReadOrFavoriteArticleToLangDto> {
+        val lang = langEnum?.lang?.let { langService.getById(it) ?: throw LangNotFoundException() }
+
+        return favoriteArticleForLangService.findAllByUserIdAndLangId(
+                userId = user.id!!,
+                langId = lang?.id,
+                offset = offset,
+                limit = limit
+        )
+    }
+
+    @PostMapping("/add")
+    fun add(
+            @RequestParam(value = "articleToLangId") articleToLangId: Long,
+            @AuthenticationPrincipal user: User
+    ): FavoriteArticleByLang {
+        val alreadySavedFavoriteArticle = favoriteArticleForLangService
+                .findByArticleToLangIdAndUserId(
+                        articleToLangId = articleToLangId,
+                        userId = user.id!!
+                )
+        if (alreadySavedFavoriteArticle == null) {
+            return favoriteArticleForLangService
+                    .save(FavoriteArticleByLang(articleToLangId = articleToLangId, userId = user.id))
+        } else {
+            throw FavoriteArticleByLangAlreadyExistsException()
+        }
+    }
+
+    @DeleteMapping("/delete")
+    fun delete(
+            @RequestParam(value = "articleToLangId") articleToLangId: Long,
+            @AuthenticationPrincipal user: User
+    ): FavoriteArticleByLang {
+        val alreadySavedFavoriteArticle = favoriteArticleForLangService
+                .findByArticleToLangIdAndUserId(
+                        articleToLangId = articleToLangId,
+                        userId = user.id!!
+                )
+        if (alreadySavedFavoriteArticle != null) {
+            favoriteArticleForLangService.deleteById(alreadySavedFavoriteArticle.id!!)
+            return alreadySavedFavoriteArticle
+        } else {
+            throw FavoriteArticleByLangNotFoundException()
+        }
+    }
+}
--- src/main/java/ru/kuchanov/scpreaderapi/service/article/favorite/FavoriteArticleForLangServiceImpl.kt
+++ src/main/java/ru/kuchanov/scpreaderapi/service/article/favorite/FavoriteArticleForLangServiceImpl.kt
@@ -1,21 +1,74 @@
 package ru.kuchanov.scpreaderapi.service.article.favorite
 
 import org.springframework.stereotype.Service
-import ru.kuchanov.scpreaderapi.bean.articles.favorite.FavoriteArticlesByLang
+import ru.kuchanov.scpreaderapi.bean.articles.favorite.FavoriteArticleByLang
+import ru.kuchanov.scpreaderapi.model.dto.article.ReadOrFavoriteArticleProjection
+import ru.kuchanov.scpreaderapi.model.dto.article.ReadOrFavoriteArticleToLangDto
 import ru.kuchanov.scpreaderapi.repository.article.favorite.FavoriteArticlesForLangRepository
+import ru.kuchanov.scpreaderapi.repository.article.tags.TagForLangRepository
+import ru.kuchanov.scpreaderapi.service.article.image.ArticlesImagesService
+import ru.kuchanov.scpreaderapi.service.article.type.ArticleAndArticleTypeService
 
 
 @Service
 class FavoriteArticleForLangServiceImpl constructor(
-        val repository: FavoriteArticlesForLangRepository
+        val repository: FavoriteArticlesForLangRepository,
+        val imagesService: ArticlesImagesService,
+        val tagsForLangRepository: TagForLangRepository,
+        val articleAndArticleTypeService: ArticleAndArticleTypeService
 ) : FavoriteArticleForLangService {
 
     override fun deleteById(id: Long) =
             repository.deleteById(id)
 
-    override fun save(article: FavoriteArticlesByLang): FavoriteArticlesByLang =
+    override fun save(article: FavoriteArticleByLang): FavoriteArticleByLang =
             repository.save(article)
 
     override fun getFavoriteArticleForArticleIdLangIdAndUserId(articleToLangId: Long, userId: Long) =
             repository.findByArticleToLangIdAndUserId(articleToLangId, userId)
+
+    override fun findByArticleToLangIdAndUserId(articleToLangId: Long, userId: Long): FavoriteArticleByLang? =
+            repository.findByArticleToLangIdAndUserId(articleToLangId, userId)
+
+    override fun findAllByUserIdAndLangId(
+            userId: Long,
+            langId: String?,
+            offset: Int,
+            limit: Int
+    ): List<ReadOrFavoriteArticleToLangDto> =
+            if (langId != null) {
+                repository.findAllByUserIdAndLangId(userId, langId, offset, limit)
+            } else {
+                repository.findAllByUserId(userId, offset, limit)
+            }
+                    .map { it.toDto().withImages().withTags().withType() }
+
+
+    fun ReadOrFavoriteArticleProjection.toDto() =
+            ReadOrFavoriteArticleToLangDto(
+                    id = id,
+                    statusChangedDate = statusChangedDate,
+                    articleToLangId = articleToLangId,
+                    articleId = articleId,
+                    langId = langId,
+                    urlRelative = urlRelative,
+                    rating = rating,
+                    title = title,
+                    createdOnSite = createdOnSite,
+                    hasIframeTag = hasIframeTag
+            )
+
+    fun ReadOrFavoriteArticleToLangDto.withImages(): ReadOrFavoriteArticleToLangDto =
+            this.apply { imageUrls = imagesService.findAllByArticleForLangId(articleForLangId = articleToLangId) }
+
+    fun ReadOrFavoriteArticleToLangDto.withTags(): ReadOrFavoriteArticleToLangDto =
+            this.apply {
+                tagsForLang = tagsForLangRepository.getAllForLangIdAndArticleForLangIdAsDto(
+                        langId = langId,
+                        articleForLangId = articleToLangId
+                )
+            }
+
+    fun ReadOrFavoriteArticleToLangDto.withType(): ReadOrFavoriteArticleToLangDto =
+            this.apply { articleTypeDto = articleAndArticleTypeService.getByArticleIdAndLangIdAsDto(articleId, langId) }
 }
