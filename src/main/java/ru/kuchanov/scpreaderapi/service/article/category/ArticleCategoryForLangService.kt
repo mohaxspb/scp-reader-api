ebff4282f63f2842bb22727c3fe6ae5ef2abd8c5
mohax <mohax.spb@gmail.com>
2020-02-14 18:03:09 +0300

    Create controller to get downloaded articles by category and lang count.

--- src/main/java/ru/kuchanov/scpreaderapi/service/article/category/ArticleCategoryForLangService.kt
+++ src/main/java/ru/kuchanov/scpreaderapi/service/article/category/ArticleCategoryForLangService.kt
@@ -1,9 +1,12 @@
 package ru.kuchanov.scpreaderapi.service.article.category
 
 import ru.kuchanov.scpreaderapi.bean.articles.category.ArticleCategoryForLang
+import ru.kuchanov.scpreaderapi.model.dto.article.ArticleCategoryToLangProjection
 
 
 interface ArticleCategoryForLangService {
+    fun findAllByLangId(langId: String): List<ArticleCategoryToLangProjection>
+
     fun findByLangIdAndArticleCategoryId(langId: String, articleCategoryId: Long): ArticleCategoryForLang?
 
     fun findByLangIdAndSiteUrl(langId: String, siteUrl: String): ArticleCategoryForLang?
