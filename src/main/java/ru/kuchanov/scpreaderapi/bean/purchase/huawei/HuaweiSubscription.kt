65afdbe5adf291f22540b840b8b851a1ca766e26
mohax <mohax.spb@gmail.com>
2020-10-25 16:30:08 +0300

    Create migration and db model for huawei subscription.

---
+++ src/main/java/ru/kuchanov/scpreaderapi/bean/purchase/huawei/HuaweiSubscription.kt
@@ -0,0 +1,99 @@
+package ru.kuchanov.scpreaderapi.bean.purchase.huawei
+
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.sql.Timestamp
+import javax.persistence.*
+
+
+/**
+ * [entity documentation][https://developer.huawei.com/consumer/en/doc/development/HMS-References/iap-InAppPurchaseDetails-v4]
+ */
+@Entity
+@Table(name = "huawei_subscriptions")
+data class HuaweiSubscription(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long? = null,
+
+        /**
+         * 0: consumable
+         * 1: non-consumable
+         * 2: renewable subscription
+         * 3: non-renewable subscription
+         */
+        val kind: Int,
+
+        @Column(name = "product_id", columnDefinition = "TEXT")
+        val productId: String? = null,
+        @Column(name = "product_name", columnDefinition = "TEXT")
+        val productName: String? = null,
+
+        @Column(columnDefinition = "TEXT")
+        val country: String? = null,
+        @Column(columnDefinition = "TEXT")
+        val currency: String? = null,
+
+        /**
+         * Value after the actual price of a product is multiplied by 100.
+         * The actual price is accurate to two decimal places.
+         * For example, if the value of this parameter is 501, the actual product price is 5.01.
+         */
+        val price: Int? = null,
+
+        /**
+         * This parameter uniquely identifies the mapping between a product and a user.
+         * It does not change when the subscription is renewed
+         */
+        @Column(name = "subscription_id")
+        val subscriptionId: String,
+        /**
+         * ID of the subscription group to which a subscription belongs.
+         */
+        @Column(name = "product_group")
+        val productGroup: String,
+        @Column(name = "sub_is_valid")
+        val subIsValid: Boolean,
+        @Column(name = "auto_renewing")
+        val autoRenewing: Boolean,
+        /**
+         * can be filled if this subscription is switched from another
+         */
+        @Column(name = "ori_subscription_id")
+        val oriSubscriptionId: String? = null,
+
+        @Column(name = "account_flag", columnDefinition = "TEXT")
+        val accountFlag: String? = null,
+
+        @Column(name = "purchase_state")
+        val purchaseState: Int? = null,
+
+        @Column(name = "android_package")
+        val androidPackage: String,
+        @Column(name = "purchase_token")
+        val purchaseToken: String,
+        /**
+         * token of previously canceled subscription or one from witch this one was upgraded
+         */
+        @Column(name = "linked_purchase_token")
+        var linkedPurchaseToken: String? = null,
+
+        @Column(name = "expiry_time_millis")
+        var expiryTimeMillis: Timestamp? = null,
+        @Column(name = "start_time_millis")
+        var startTimeMillis: Timestamp? = null,
+        @Column(name = "user_cancellation_time_millis")
+        var userCancellationTimeMillis: Timestamp? = null,
+        @Column(name = "price_amount_micros")
+        val priceAmountMicros: Long? = null,
+        @Column(name = "price_currency_code")
+        val priceCurrencyCode: String? = null,
+        @Column(name = "order_id")
+        var orderId: String,
+
+        //dates
+        @field:CreationTimestamp
+        val created: Timestamp? = null,
+        @field:UpdateTimestamp
+        val updated: Timestamp? = null
+)
---
+++ src/main/resources/migration/V16__create_huawei_purchase_tables.sql
@@ -0,0 +1,120 @@
+create table if not exists huawei_products
+(
+    id                   bigserial not null,
+    android_package      text,
+    --0: consumable
+    --1: non-consumable
+    --2: renewable subscription
+    --3: non-renewable subscription
+    kind                 integer   not null,
+
+    order_id             text,
+    purchase_state       integer,
+    purchase_time_millis timestamp,
+    purchase_token       text,
+    purchase_type        integer,
+
+    product_id           text,
+    product_name         text,
+
+    consumption_state    integer,
+
+    country              text,
+    currency             text,
+    --Value after the actual price of a product is multiplied by 100.
+    -- The actual price is accurate to two decimal places.
+    -- For example, if the value of this parameter is 501, the actual product price is 5.01.
+    price                integer,
+
+    account_flag         text,
+
+    created              timestamp,
+    updated              timestamp,
+    primary key (id)
+);
+
+create table if not exists huawei_subscriptions
+(
+    id                            bigserial not null,
+    android_package               text,
+    --0: consumable
+    --1: non-consumable
+    --2: renewable subscription
+    --3: non-renewable subscription
+    kind                          integer   not null,
+
+    order_id                      text,
+    purchase_state                integer,
+
+    price_amount_micros           bigint,
+    price_currency_code           text,
+
+    purchase_token                text,
+
+    start_time_millis             timestamp,
+    user_cancellation_time_millis timestamp,
+
+    product_id                    text,
+    product_name                  text,
+
+    country                       text,
+    currency                      text,
+    --Value after the actual price of a product is multiplied by 100.
+    -- The actual price is accurate to two decimal places.
+    -- For example, if the value of this parameter is 501, the actual product price is 5.01.
+    price                         integer,
+
+    --subs fields
+    --This parameter uniquely identifies the mapping between a product and a user. It does not change when the subscription is renewed
+    subscription_id               text      not null,
+    --ID of the subscription group to which a subscription belongs.
+    product_group                 text      not null,
+
+    sub_is_valid                  boolean,
+    auto_renewing                 boolean,
+    --can be filled if this subscription is switched from another
+    ori_subscription_id           text,
+
+    expiry_time_millis            timestamp,
+    linked_purchase_token         text,
+
+    account_flag                  text,
+
+    created                       timestamp,
+    updated                       timestamp,
+    primary key (id)
+);
+
+
+create table if not exists users_huawei_products
+(
+    huawei_product_id bigint not null,
+    user_id           bigint not null,
+    created           timestamp,
+    updated           timestamp,
+    primary key (huawei_product_id, user_id)
+);
+
+ALTER TABLE users_huawei_products
+    drop constraint IF EXISTS fk_user_id__to__users CASCADE;
+alter table users_huawei_products
+    add constraint fk_user_id__to__users
+        foreign key (user_id)
+            REFERENCES users (id);
+
+
+create table if not exists users_huawei_subscriptions
+(
+    huawei_subscription_id bigint not null,
+    user_id                bigint not null,
+    created                timestamp,
+    updated                timestamp,
+    primary key (huawei_subscription_id, user_id)
+);
+
+ALTER TABLE users_huawei_subscriptions
+    drop constraint IF EXISTS fk_user_id__to__users CASCADE;
+alter table users_huawei_subscriptions
+    add constraint fk_user_id__to__users
+        foreign key (user_id)
+            REFERENCES users (id);
