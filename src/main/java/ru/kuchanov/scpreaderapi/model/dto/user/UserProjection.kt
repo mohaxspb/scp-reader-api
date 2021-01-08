cc255bec96acad2b6e4df0d2e56353594bce64f8
mohax <mohax.spb@gmail.com>
2021-01-08 14:17:27 +0300

    Refactoring.

--- src/main/java/ru/kuchanov/scpreaderapi/model/dto/user/UserProjection.kt
+++ src/main/java/ru/kuchanov/scpreaderapi/model/dto/user/UserProjection.kt
@@ -7,15 +7,18 @@ interface UserProjection {
     val fullName: String?
     val avatar: String?
     var score: Int
+
     //monetization
     val adsDisabledEndDate: Timestamp?
     val offlineLimitDisabledEndDate: Timestamp?
     val adsDisabled: Boolean
     val offlineLimitDisabled: Boolean
+
     //level
     val levelNum: Int
     val scoreToNextLevel: Int
     val curLevelScore: Int
+
     //articles
     val numOfReadArticles: Int
 }
