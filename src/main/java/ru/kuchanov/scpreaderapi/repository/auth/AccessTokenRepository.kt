58e702b938bd18dfdce87feb26e0634adb3e23aa
mohax <mohax.spb@gmail.com>
2021-02-28 20:59:14 +0300

    Create service and its impl for AccessToken DB model.

--- src/main/java/ru/kuchanov/scpreaderapi/repository/auth/AccessTokenRepository.kt
+++ src/main/java/ru/kuchanov/scpreaderapi/repository/auth/AccessTokenRepository.kt
@@ -3,4 +3,7 @@ package ru.kuchanov.scpreaderapi.repository.auth
 import org.springframework.data.jpa.repository.JpaRepository
 import ru.kuchanov.scpreaderapi.bean.auth.OAuthAccessToken
 
-interface AccessTokenRepository : JpaRepository<OAuthAccessToken, String>
+interface AccessTokenRepository : JpaRepository<OAuthAccessToken, String> {
+
+    fun findAllByClientId(clientId: String): List<OAuthAccessToken>
+}
