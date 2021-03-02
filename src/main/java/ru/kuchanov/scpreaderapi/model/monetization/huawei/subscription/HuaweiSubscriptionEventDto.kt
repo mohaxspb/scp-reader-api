6156d441be1ae190988718a05410cdf6ca7db47c
mohax <mohax.spb@gmail.com>
2021-03-02 18:39:04 +0300

    Create request/response models for huawei subscription events.

---
+++ src/main/java/ru/kuchanov/scpreaderapi/model/monetization/huawei/subscription/HuaweiSubscriptionEventDto.kt
@@ -0,0 +1,10 @@
+package ru.kuchanov.scpreaderapi.model.monetization.huawei.subscription
+
+/**
+ * [documentation](https://developer.huawei.com/consumer/en/doc/development/HMSCore-References-V5/api-notifications-about-subscription-events-0000001050706084-V5#EN-US_TOPIC_0000001050706084__table15297548123520)
+ */
+data class HuaweiSubscriptionEventDto(
+        val statusUpdateNotification: String,
+        @Suppress("SpellCheckingInspection")
+        val notifycationSignature: String
+)
