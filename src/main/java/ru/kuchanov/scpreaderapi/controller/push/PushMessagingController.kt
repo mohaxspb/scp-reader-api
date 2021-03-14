003bd6a98236dfda7f075464a758371c7829116d
mohax <mohax.spb@gmail.com>
2021-03-14 17:07:04 +0300

    Use service to send push messages to all providers in push controller.

--- src/main/java/ru/kuchanov/scpreaderapi/controller/push/PushMessagingController.kt
+++ src/main/java/ru/kuchanov/scpreaderapi/controller/push/PushMessagingController.kt
@@ -5,17 +5,16 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 import ru.kuchanov.scpreaderapi.ScpReaderConstants
 import ru.kuchanov.scpreaderapi.bean.users.User
+import ru.kuchanov.scpreaderapi.service.push.AllProvidersMessagingService
 import ru.kuchanov.scpreaderapi.service.push.PushMessageService
-import ru.kuchanov.scpreaderapi.service.push.firebase.FirebaseMessagingService
-import ru.kuchanov.scpreaderapi.service.push.huawei.HuaweiMessagingService
+import ru.kuchanov.scpreaderapi.service.push.PushSendResult
 
 
 @RestController
 @RequestMapping("/" + ScpReaderConstants.Path.PUSH + "/" + ScpReaderConstants.Path.MESSAGING)
 class PushMessagingController @Autowired constructor(
-        val fcmService: FirebaseMessagingService,
-        val huaweiMessagingService: HuaweiMessagingService,
-        val pushMessageService: PushMessageService
+        val pushMessageService: PushMessageService,
+        val allProvidersMessagingService: AllProvidersMessagingService
 ) {
 
     @GetMapping("/send/topic")
@@ -26,14 +25,15 @@ class PushMessagingController @Autowired constructor(
             @RequestParam(value = "message") message: String,
             @RequestParam(value = "url") url: String?,
             @AuthenticationPrincipal user: User
-    ) = fcmService.sendMessageToTopic(topicName, type, title, message, url, user) //todo use huawei
+    ): List<PushSendResult> =
+            allProvidersMessagingService.sendMessageToTopic(topicName, type, title, message, url, user)
 
     @GetMapping("/send/topic/{id}")
     fun sendToTopic(
             @PathVariable(value = "id") id: Long,
             @RequestParam(value = "topicName") topicName: String,
             @AuthenticationPrincipal user: User
-    ) = fcmService.sendMessageToTopicById(topicName, id, user) //todo use huawei
+    ): List<PushSendResult> = allProvidersMessagingService.sendMessageToTopicById(topicName, id, user)
 
     @GetMapping("/all/byTypes")
     fun getAllByTypes(
