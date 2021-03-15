a4578f2668d15d1baba83d3e784ec46e305e315e
mohax <mohax.spb@gmail.com>
2021-03-15 19:07:35 +0300

    Realize sending topic message by Huawei API.

--- src/main/java/ru/kuchanov/scpreaderapi/service/push/huawei/HuaweiMessagingService.kt
+++ src/main/java/ru/kuchanov/scpreaderapi/service/push/huawei/HuaweiMessagingService.kt
@@ -2,18 +2,26 @@ package ru.kuchanov.scpreaderapi.service.push.huawei
 
 import org.slf4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import ru.kuchanov.scpreaderapi.ScpReaderConstants
 import ru.kuchanov.scpreaderapi.bean.firebase.push.PushMessage
 import ru.kuchanov.scpreaderapi.bean.firebase.push.PushMessageNotFoundException
 import ru.kuchanov.scpreaderapi.bean.users.User
+import ru.kuchanov.scpreaderapi.configuration.HuaweiApiConfiguration.Companion.HUAWEI_PUSH_SENDING_SUCCESS_CODE
+import ru.kuchanov.scpreaderapi.model.exception.ScpServerException
+import ru.kuchanov.scpreaderapi.model.huawei.push.HuaweiPushMessage
+import ru.kuchanov.scpreaderapi.network.HuaweiPushApi
 import ru.kuchanov.scpreaderapi.service.push.PushMessageService
 import ru.kuchanov.scpreaderapi.service.push.PushProviderMessagingService
+import java.net.HttpURLConnection
 
 @Service
 class HuaweiMessagingService @Autowired constructor(
-        val log: Logger,
-        val pushMessageService: PushMessageService
+        private val log: Logger,
+        private val pushMessageService: PushMessageService,
+        private val huaweiPushApi: HuaweiPushApi,
+        @Value("\${my.api.huawei.client_id}") private val huaweiClientId: String
 ) : PushProviderMessagingService {
 
     override fun sendMessageToTopic(
@@ -51,6 +59,54 @@ class HuaweiMessagingService @Autowired constructor(
             topicName: String,
             pushMessage: PushMessage
     ): PushMessage {
-        TODO("Not yet implemented")
+        val data = pushMessageToMap(pushMessage).asIterable().joinToString(
+                prefix = "{",
+                postfix = "}",
+                separator = ", ",
+                transform = { (key, value) -> "'$key':'$value'" }
+        )
+
+        try {
+            val result = huaweiPushApi.send(
+                    applicationId = huaweiClientId,
+                    huaweiPushMessage = HuaweiPushMessage(
+                            message = HuaweiPushMessage.Message(
+                                    data = data,
+                                    topic = topicName
+                            )
+                    )
+            ).execute()
+            if (result.isSuccessful && result.code() == HttpURLConnection.HTTP_OK) {
+                //check huawei response
+                val response = result.body()
+                        ?: throw ScpServerException(
+                                "Body is null while send push to topic to Huawei",
+                                NullPointerException()
+                        )
+                if (response.code == HUAWEI_PUSH_SENDING_SUCCESS_CODE) {
+                    return pushMessageService.save(pushMessage.apply { sent = true })
+                } else {
+                    val errorMessage = """
+                         Send push to Huawei topic failed! 
+                         Huawei API response: $response
+                    """.trimIndent()
+                    log.error(errorMessage)
+                    pushMessageService.save(pushMessage.apply { sent = false })
+                    throw ScpServerException(errorMessage)
+                }
+            } else {
+                val errorMessage = """
+                    Send push to Huawei topic failed! 
+                    Http code: ${result.code()}, http errorBody: ${result.errorBody()?.string()}
+                    """.trimIndent()
+                log.error(errorMessage)
+                pushMessageService.save(pushMessage.apply { sent = false })
+                throw ScpServerException(errorMessage)
+            }
+        } catch (e: Exception) {
+            log.error(e.message, e)
+            pushMessageService.save(pushMessage.apply { sent = false })
+            throw ScpServerException(e.message, e)
+        }
     }
 }
