da01e17d0e9cc2773ad03c93f10af1fb30b22195
mohax <mohax.spb@gmail.com>
2021-03-17 18:33:00 +0300

    Add unique constraint to users_to_push_tokens table.

--- src/main/resources/migration/V19__create_users_to_push_tokens_table.sql
+++ src/main/resources/migration/V19__create_users_to_push_tokens_table.sql
@@ -17,3 +17,8 @@ alter table users_to_push_tokens
     add constraint fk_user_id__to__users
         foreign key (user_id)
             REFERENCES scp_reader.public.users (id) ON DELETE CASCADE;
+
+alter table users_to_push_tokens
+    drop constraint if exists user_provider_token_unique;
+alter table users_to_push_tokens
+    add constraint user_provider_token_unique unique (user_id, push_token_provider, push_token_value);
