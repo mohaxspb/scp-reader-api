32d818b0847820a84c58dc37371ab5ba2b0b90b8
mohax <mohax.spb@gmail.com>
2021-05-06 19:33:12 +0300

    Refactoring.

--- src/main/java/ru/kuchanov/scpreaderapi/controller/article/MassArticleDownloadController.kt
+++ src/main/java/ru/kuchanov/scpreaderapi/controller/article/MassArticleDownloadController.kt
@@ -1,14 +1,12 @@
 package ru.kuchanov.scpreaderapi.controller.article
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.kuchanov.scpreaderapi.ScpReaderConstants
 import ru.kuchanov.scpreaderapi.bean.users.LangNotFoundException
-import ru.kuchanov.scpreaderapi.bean.users.User
 import ru.kuchanov.scpreaderapi.model.dto.article.ArticleCategoryToLangProjection
 import ru.kuchanov.scpreaderapi.service.article.category.ArticleCategoryForLangService
 import ru.kuchanov.scpreaderapi.service.users.LangService
@@ -17,8 +15,8 @@ import ru.kuchanov.scpreaderapi.service.users.LangService
 @RestController
 @RequestMapping("/" + ScpReaderConstants.Path.ARTICLE + "/" + ScpReaderConstants.Path.DOWNLOAD)
 class MassArticleDownloadController @Autowired constructor(
-        val categoryForLangService: ArticleCategoryForLangService,
-        val langService: LangService
+        private val categoryForLangService: ArticleCategoryForLangService,
+        private val langService: LangService
 ) {
 
     @GetMapping("{langEnum}/categories/all")
