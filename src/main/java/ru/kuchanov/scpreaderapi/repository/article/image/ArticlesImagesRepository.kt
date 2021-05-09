4c43692f2d239062615f2f8afd6d8796687027df
mohax <mohax.spb@gmail.com>
2021-05-09 16:33:37 +0300

    Fully update articles images during parsing. Remove duplicated images. Add unique constraint (via index) on articles_images table.

--- src/main/java/ru/kuchanov/scpreaderapi/repository/article/image/ArticlesImagesRepository.kt
+++ src/main/java/ru/kuchanov/scpreaderapi/repository/article/image/ArticlesImagesRepository.kt
@@ -1,17 +1,19 @@
 package ru.kuchanov.scpreaderapi.repository.article.image
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import ru.kuchanov.scpreaderapi.bean.articles.image.ArticlesImages
 import ru.kuchanov.scpreaderapi.model.dto.article.ArticlesImagesDto
 import ru.kuchanov.scpreaderapi.model.dto.article.ArticlesImagesProjection
+import javax.transaction.Transactional
 
 interface ArticlesImagesRepository : JpaRepository<ArticlesImages, Long> {
 
     fun findAllByArticleForLangId(articleForLangId: Long): List<ArticlesImagesDto>
 
-    @Query("select a from ArticlesImages a where a.articleForLangId = :articleForLangId")
-    fun findAllByArticleForLangIdFull(articleForLangId: Long): List<ArticlesImages>
+    fun findByArticleForLangIdAndUrl(articleForLangId: Long, url: String): ArticlesImages?
+
+    @Transactional
+    fun deleteAllByArticleForLangId(articleForLangId: Long)
 
     @Suppress("SpringDataRepositoryMethodReturnTypeInspection")
     fun findAllByArticleForLangIdIn(articleForLangIds: List<Long>): List<ArticlesImagesProjection>
