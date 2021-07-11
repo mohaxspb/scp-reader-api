78387c20f24b24e484366ac73ec3a475c2836e82
mohax <mohax.spb@gmail.com>
2021-07-12 00:24:21 +0300

    Delete unused code.

--- src/main/java/ru/kuchanov/scpreaderapi/model/monetization/google/subscription/subevent/GoogleSubscriptionEventResponse.kt
+++
@@ -1,9 +0,0 @@
-package ru.kuchanov.scpreaderapi.model.monetization.google.subscription.subevent
-
-/**
- * [documentation](https://developer.huawei.com/consumer/en/doc/development/HMSCore-References-V5/api-notifications-about-subscription-events-0000001050706084-V5#EN-US_TOPIC_0000001050706084__table15297548123520)
- */
-data class GoogleSubscriptionEventResponse(
-        val errorCode: String = "0",
-        val errorMsg: String = "success"
-)
