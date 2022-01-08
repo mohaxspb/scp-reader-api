a919447cbdd63cba8913d275ca0f1c81ab92a20b
mohax <mohax.spb@gmail.com>
2022-01-08 21:15:44 +0300

    Update migration with adding article title to ts.

--- src/main/resources/migration/V30__search_feature.sql
+++ src/main/resources/migration/V30__search_feature.sql
@@ -38,6 +38,16 @@ from (
      ) as c(lang_code, postgres_lang)
 where c.lang_code = l.lang_code;
 
+-- fill vectors for existing rows
+INSERT INTO article_to_lang_text_parts_vector(text_part_id, vector)
+select t.id,
+       coalesce(to_tsvector(l.postgres_lang::regconfig, t.data), to_tsvector(l.postgres_lang::regconfig, ''))
+from article_to_lang_text_parts t
+         join articles_langs al on t.article_to_lang_id = al.id
+         join langs l on al.lang_id = l.id
+where t.type in ('TEXT', 'IMAGE_TITLE', 'TABLE')
+ON CONFLICT DO NOTHING;
+
 -- trigger, that inserts vector for each text part
 CREATE OR REPLACE FUNCTION insert_vector() RETURNS TRIGGER AS
 $BODY$
@@ -56,8 +66,8 @@ END;
 $BODY$
     language plpgsql;
 
-DROP TRIGGER IF EXISTS text_parts_vector_insert on article_to_lang_text_parts;
 
+DROP TRIGGER IF EXISTS text_parts_vector_insert on article_to_lang_text_parts;
 CREATE TRIGGER text_parts_vector_insert
     AFTER INSERT
     ON article_to_lang_text_parts
@@ -65,3 +75,62 @@ CREATE TRIGGER text_parts_vector_insert
     WHEN (new.type = 'TEXT' or new.type = 'IMAGE_TITLE')
 EXECUTE PROCEDURE
     insert_vector();
+
+
+-- titles
+create table if not exists article_to_lang_titles_vector
+(
+    id                 bigserial not null,
+
+    article_to_lang_id bigint    not null,
+    vector             tsvector  not null,
+    primary key (id)
+);
+
+ALTER TABLE article_to_lang_titles_vector
+    drop constraint IF EXISTS fk_text_part_id__to__article_to_langs CASCADE;
+alter table article_to_lang_titles_vector
+    add constraint fk_text_part_id__to__article_to_langs
+        foreign key (article_to_lang_id)
+            REFERENCES articles_langs (id) ON DELETE CASCADE;
+
+drop index if exists idx_gin_article_to_lang_titles;
+CREATE INDEX idx_gin_article_to_lang_titles
+    ON article_to_lang_titles_vector
+        USING gin ("vector");
+
+-- fill vectors for existing rows
+INSERT INTO article_to_lang_titles_vector(article_to_lang_id, vector)
+select al.id,
+       coalesce(to_tsvector(l.postgres_lang::regconfig, al.title), to_tsvector(l.postgres_lang::regconfig, ''))
+from articles_langs al
+         join langs l on al.lang_id = l.id
+ON CONFLICT DO NOTHING;
+
+
+-- trigger, that inserts vector for each text part
+CREATE OR REPLACE FUNCTION insert_titles_vector() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    INSERT INTO article_to_lang_titles_vector(article_to_lang_id, vector)
+    VALUES (new.id,
+            to_tsvector(
+                    (select langs.postgres_lang
+                     from langs
+                     where id = new.lang_id)::regconfig,
+                    new.title
+                ));
+
+    RETURN new;
+END;
+$BODY$
+    language plpgsql;
+
+
+DROP TRIGGER IF EXISTS article_to_langs_vector_insert on articles_langs;
+CREATE TRIGGER article_to_langs_vector_insert
+    AFTER INSERT
+    ON articles_langs
+    FOR EACH ROW
+EXECUTE PROCEDURE
+    insert_titles_vector();
