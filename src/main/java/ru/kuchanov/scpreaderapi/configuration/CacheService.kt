f20c6e99650f240e43b44ac7d4e21bcadc2b58b2
mohax <mohax.spb@gmail.com>
2023-03-19 19:51:54 +0300

    Try-catch for cache population. Cache population status showing.

--- src/main/java/ru/kuchanov/scpreaderapi/configuration/CacheService.kt
+++ src/main/java/ru/kuchanov/scpreaderapi/configuration/CacheService.kt
@@ -13,6 +13,7 @@ import ru.kuchanov.scpreaderapi.ScpReaderConstants
 import ru.kuchanov.scpreaderapi.ScpReaderConstants.Cache.Keys.ARTICLE_TO_LANG_DTO_BY_ID
 import ru.kuchanov.scpreaderapi.ScpReaderConstants.Cache.Keys.ARTICLE_TO_LANG_DTO_BY_URL_RELATIVE_AND_LANG
 import ru.kuchanov.scpreaderapi.ScpReaderConstants.Cache.Keys.CATEGORIES_ARTICLES
+import ru.kuchanov.scpreaderapi.ScpReaderConstants.Cache.Keys.SEARCH_RESULTS_CACHE
 import ru.kuchanov.scpreaderapi.bean.settings.ServerSettings
 import ru.kuchanov.scpreaderapi.bean.settings.ServerSettingsNotFoundException
 import ru.kuchanov.scpreaderapi.bean.users.LangNotFoundException
@@ -20,85 +21,131 @@ import ru.kuchanov.scpreaderapi.service.article.ArticleForLangService
 import ru.kuchanov.scpreaderapi.service.article.category.ArticleCategoryForLangService
 import ru.kuchanov.scpreaderapi.service.settings.ServerSettingsService
 import ru.kuchanov.scpreaderapi.service.users.LangService
+import ru.kuchanov.scpreaderapi.utils.ErrorUtils
 import ru.kuchanov.scpreaderapi.utils.millisToMinutesAndSeconds
+import java.util.concurrent.atomic.AtomicReference
 
 @Service
 class CacheService @Autowired constructor(
-        private val articleForLangService: ArticleForLangService,
-        private val langService: LangService,
-        private val categoryForLangService: ArticleCategoryForLangService,
-        private val cacheManager: CacheManager,
-        private val serverSettingsService: ServerSettingsService,
-        @Qualifier(Application.CACHE_LOGGER) private val log: Logger
+    private val articleForLangService: ArticleForLangService,
+    private val langService: LangService,
+    private val categoryForLangService: ArticleCategoryForLangService,
+    private val cacheManager: CacheManager,
+    private val serverSettingsService: ServerSettingsService,
+    private val errorUtils: ErrorUtils,
+    @Qualifier(Application.CACHE_LOGGER) private val log: Logger
 ) {
+
+    sealed class CacheStatus {
+        object NotPopulated : CacheStatus()
+        object Populating : CacheStatus()
+        data class Populated(
+            val categoriesArticlesCacheSize: Long,
+            val articlesByIdCacheSize: Long,
+            val articlesByUrlAndLangCacheSize: Long,
+            val searchResultsSize: Long
+        ) : CacheStatus()
+    }
+
+    val cacheStatus: AtomicReference<CacheStatus> = AtomicReference(CacheStatus.NotPopulated)
 
     @Async
     fun populateCache() {
-        val categoriesArticlesCache = cacheManager.getCache(CATEGORIES_ARTICLES)
-        log.error("cache population START")
-        val startTime = System.currentTimeMillis()
-        ScpReaderConstants.Firebase.FirebaseInstance.values().forEach { langEnum ->
-            val startTimeLang = System.currentTimeMillis()
-            log.error("==populate cache for $langEnum START")
-            val lang = langEnum.lang.let { langService.getById(it) ?: throw LangNotFoundException() }
-            //todo exclude recent and rated lists
-            categoryForLangService.findAllByLangId(lang.id).forEach categoryToLang@{ categoryToLang ->
-                log.error("====categoriesArticlesCache $langEnum ${categoryToLang.defaultTitle} START")
-                val articleCategoryToLang = categoryForLangService.findByLangIdAndArticleCategoryId(
+        cacheStatus.set(CacheStatus.Populating)
+        try {
+            val categoriesArticlesCache = cacheManager.getCache(CATEGORIES_ARTICLES)
+            log.error("cache population START")
+            val startTime = System.currentTimeMillis()
+            ScpReaderConstants.Firebase.FirebaseInstance.values().forEach { langEnum ->
+                val startTimeLang = System.currentTimeMillis()
+                log.error("==populate cache for $langEnum START")
+                val lang = langEnum.lang.let { langService.getById(it) ?: throw LangNotFoundException() }
+                //todo exclude recent and rated lists
+                categoryForLangService.findAllByLangId(lang.id).forEach categoryToLang@{ categoryToLang ->
+                    log.error("====categoriesArticlesCache $langEnum ${categoryToLang.defaultTitle} START")
+                    val articleCategoryToLang = categoryForLangService.findByLangIdAndArticleCategoryId(
                         langId = lang.id,
                         articleCategoryId = categoryToLang.articleCategoryId
-                ) ?: return@categoryToLang
+                    ) ?: return@categoryToLang
 
-                val dataToPut = articleForLangService.findAllArticlesForLangByArticleCategoryToLangId(articleCategoryToLang.id!!)
-                categoriesArticlesCache?.put(
+                    val dataToPut =
+                        articleForLangService.findAllArticlesForLangByArticleCategoryToLangId(articleCategoryToLang.id!!)
+                    categoriesArticlesCache?.put(
                         SimpleKey(langEnum, categoryToLang.articleCategoryId),
                         dataToPut
-                )
+                    )
 
-                //put articles with text parts
-                populateArticlesWithTextByIds(langEnum, dataToPut.map { it.id })
-                log.error("====categoriesArticlesCache $langEnum ${categoryToLang.defaultTitle} END")
-            }
+                    //put articles with text parts
+                    populateArticlesWithTextByIds(langEnum, dataToPut.map { it.id })
+                    log.error("====categoriesArticlesCache $langEnum ${categoryToLang.defaultTitle} END")
+                }
 
-            log.error("==populate recent $langEnum with text START")
-            //fill recent and popular articles with text cache
-            val recentCacheInitialSize = serverSettingsService.findByKey(ServerSettings.Key.MOST_RECENT_ARTICLES_CACHE_SIZE.name)
-                    ?: throw ServerSettingsNotFoundException("MOST_RECENT_ARTICLES_CACHE_SIZE not found!")
+                log.error("==populate recent $langEnum with text START")
+                //fill recent and popular articles with text cache
+                val recentCacheInitialSize =
+                    serverSettingsService.findByKey(ServerSettings.Key.MOST_RECENT_ARTICLES_CACHE_SIZE.name)
+                        ?: throw ServerSettingsNotFoundException("MOST_RECENT_ARTICLES_CACHE_SIZE not found!")
 
-            val recentArticlesIds: List<Long> = articleForLangService.getMostRecentArticlesForLangIds(
+                val recentArticlesIds: List<Long> = articleForLangService.getMostRecentArticlesForLangIds(
                     langEnum.lang,
                     0,
                     recentCacheInitialSize.value.toInt()
-            )
-            populateArticlesWithTextByIds(langEnum, recentArticlesIds)
-            log.error("==populate recent $langEnum with text END")
-            log.error("==populate rated $langEnum with text START")
-            val ratedCacheInitialSize = serverSettingsService.findByKey(ServerSettings.Key.MOST_RATED_ARTICLES_CACHE_SIZE.name)
-                    ?: throw ServerSettingsNotFoundException("MOST_RATED_ARTICLES_CACHE_SIZE not found!")
+                )
+                populateArticlesWithTextByIds(langEnum, recentArticlesIds)
+                log.error("==populate recent $langEnum with text END")
+                log.error("==populate rated $langEnum with text START")
+                val ratedCacheInitialSize =
+                    serverSettingsService.findByKey(ServerSettings.Key.MOST_RATED_ARTICLES_CACHE_SIZE.name)
+                        ?: throw ServerSettingsNotFoundException("MOST_RATED_ARTICLES_CACHE_SIZE not found!")
 
-            val ratedArticlesIds: List<Long> = articleForLangService.getMostRatedArticlesForLangIds(
+                val ratedArticlesIds: List<Long> = articleForLangService.getMostRatedArticlesForLangIds(
                     langEnum.lang,
                     0,
                     ratedCacheInitialSize.value.toInt()
+                )
+                populateArticlesWithTextByIds(langEnum, ratedArticlesIds)
+                log.error("==populate rated $langEnum with text END")
+                val (minutes, seconds) = millisToMinutesAndSeconds(System.currentTimeMillis() - startTimeLang)
+                log.error("==populate cache for $langEnum END. Total time (min:sec): $minutes:$seconds")
+            }
+            val (minutes, seconds) = millisToMinutesAndSeconds(System.currentTimeMillis() - startTime)
+            log.error("cache population END. Total time (min:sec): $minutes:$seconds")
+
+            val caffeinCategoriesArticlesCache = (categoriesArticlesCache as CaffeineCache).nativeCache
+            log.error("caffeinCache estimatedSize: ${caffeinCategoriesArticlesCache.estimatedSize()}")
+
+            val caffeineArticlesByIdCache =
+                (cacheManager.getCache(ARTICLE_TO_LANG_DTO_BY_ID) as CaffeineCache).nativeCache
+            log.error("caffeineArticlesByIdCache estimatedSize: ${caffeineArticlesByIdCache.estimatedSize()}")
+
+            val caffeineArticlesByLangAndUrlRelativeCache =
+                (cacheManager.getCache(ARTICLE_TO_LANG_DTO_BY_URL_RELATIVE_AND_LANG) as CaffeineCache).nativeCache
+            log.error("caffeineArticlesByLangAndUrlRelativeCache estimatedSize: ${caffeineArticlesByLangAndUrlRelativeCache.estimatedSize()}")
+
+            //todo search cache
+
+            val caffeineSearchCache =
+                (cacheManager.getCache(SEARCH_RESULTS_CACHE) as CaffeineCache).nativeCache
+            log.error("caffeineSearchCache estimatedSize: ${caffeineSearchCache.estimatedSize()}")
+
+            cacheStatus.set(
+                CacheStatus.Populated(
+                    categoriesArticlesCacheSize = caffeinCategoriesArticlesCache.estimatedSize(),
+                    articlesByIdCacheSize = caffeineArticlesByIdCache.estimatedSize(),
+                    articlesByUrlAndLangCacheSize = caffeineArticlesByLangAndUrlRelativeCache.estimatedSize(),
+                    searchResultsSize = caffeineSearchCache.estimatedSize(),
+                )
             )
-            populateArticlesWithTextByIds(langEnum, ratedArticlesIds)
-            log.error("==populate rated $langEnum with text END")
-            val (minutes, seconds) = millisToMinutesAndSeconds(System.currentTimeMillis() - startTimeLang)
-            log.error("==populate cache for $langEnum END. Total time (min:sec): $minutes:$seconds")
+        } catch (e: Exception) {
+            val stackTrace = errorUtils.stackTraceAsString(e)
+            log.error("Error while populating cache:\n$stackTrace")
+            cacheStatus.set(CacheStatus.NotPopulated)
         }
-        val (minutes, seconds) = millisToMinutesAndSeconds(System.currentTimeMillis() - startTime)
-        log.error("cache population END. Total time (min:sec): $minutes:$seconds")
-
-        val caffeinCategoriesArticlesCache = (categoriesArticlesCache as CaffeineCache).nativeCache
-        log.error("caffeinCache estimatedSize: ${caffeinCategoriesArticlesCache.estimatedSize()}")
-
-        val caffeineArticlesByIdCache = (cacheManager.getCache(ARTICLE_TO_LANG_DTO_BY_ID) as CaffeineCache).nativeCache
-        log.error("caffeineArticlesByIdCache estimatedSize: ${caffeineArticlesByIdCache.estimatedSize()}")
     }
 
     private fun populateArticlesWithTextByIds(
-            langEnum: ScpReaderConstants.Firebase.FirebaseInstance,
-            ids: List<Long>
+        langEnum: ScpReaderConstants.Firebase.FirebaseInstance,
+        ids: List<Long>
     ) {
         val articlesByIdCache = cacheManager.getCache(ARTICLE_TO_LANG_DTO_BY_ID)
         val articlesByLangAndUrlRelativeCache = cacheManager.getCache(ARTICLE_TO_LANG_DTO_BY_URL_RELATIVE_AND_LANG)
